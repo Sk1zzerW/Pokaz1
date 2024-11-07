921294f0c3835d80c6b4bbb42480415b724bd82c
olegk <kbryzhenyukpc@gmail.com>
2024-11-07 10:17:04 +0200

    Добавьте файлы проекта.

---
+++ Pokaz1/Pokaz1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void copyArray(int* src, int* dest, int size) {
+    for (int i = 0; i < size; i++) {
+        *(dest + i) = *(src + i);
+    }
+}
+
+int main() {
+    int size = 5;
+    int srcArray[] = { 1, 2, 3, 4, 5 };
+    int* destArray = new int[size];
+
+    copyArray(srcArray, destArray, size);
+
+    cout << "скопированый массив ";
+    for (int i = 0; i < size; i++) {
+        cout << destArray[i] << " ";
+    }
+    cout << endl;
+
+    delete[] destArray;
+    return 0;
+}
